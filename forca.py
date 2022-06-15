4641ad4d042ccc15b4afa821ad294666dc0ea0ae
tiago rocha <tiagoferreira.info@gmail.com>
2022-06-14 23:09:29 -0300

    Parte 01

--- forca.py
+++ forca.py
@@ -4,6 +4,14 @@ def jogar():
     print("*  Bem vindo ao jogo da Forca!  *")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while (not acertou and not enforcou):
+        print("Jogando...")
+
     print("Fim do jogo")
 
 
